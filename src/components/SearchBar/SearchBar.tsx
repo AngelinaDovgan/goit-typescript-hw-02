a5314a53a5ceabdd1ddcf1c0d6201d84b596896f
Angelina <midnightshout03@gmail.com>
2024-05-31 15:50:34 +0300

    Update SearchBar.tsx

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -2,7 +2,12 @@ import { Form, Field, Formik } from "formik";
 import toast, { Toaster } from "react-hot-toast";
 import css from './SearchBar.module.css'
 
-export default function SearchBar({ onSearch }) {
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
    
   return (
     <div>
@@ -35,3 +40,40 @@ export default function SearchBar({ onSearch }) {
     
   )
 }
+
+
+
+
+// export default function SearchBar({ onSearch }) {
+   
+//   return (
+//     <div>
+//       <Toaster />
+//       <Formik
+//         initialValues={{ query: "" }}
+//         onSubmit={(values, actions) => {
+//           if (!values.query.trim()) {
+//             toast.error("Please type your text!")
+//           } else {
+//             onSearch(values.query);
+//             toast.success("Awesome!🥳")
+//             actions.resetForm()
+//           }
+//         }}>
+        
+//         <Form>
+//           <Field
+//             type="text"
+//             name="query"
+//             placeholder="Type here..."
+//             autoComplete="off"
+//             autoFocus
+//             className={css.field}
+//           />
+//           <button type="submit" className={css.btn}>Click to start!</button>
+//         </Form>
+// </Formik>
+//         </div>
+    
+//   )
+// }
