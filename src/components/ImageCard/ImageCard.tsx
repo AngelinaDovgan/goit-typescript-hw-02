b61741e41c3714c427610eefb8e284e1e590051c
Angelina <midnightshout03@gmail.com>
2024-05-31 14:38:07 +0300

    ImageCard update

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -2,7 +2,21 @@ import css from './ImageCard.module.css';
 import { GoCheck } from "react-icons/go";
 import { MdOutlineDescription } from "react-icons/md";
 
-export default function ImageCard({ onClick, image: { urls: { small }, alt_description, user: {name} } }) {
+
+interface ImageCardProps {
+  image: {
+    urls: {
+      small: string;
+    };
+    alt_description: string;
+    user: {
+      name: string;
+    };
+  };
+  onClick: () => void;
+}
+
+const ImageCard: React.FC<ImageCardProps> = ({ onClick, image: { urls: { small }, alt_description, user: {name} } }) => {
     const handleClick = () => {
         onClick && onClick();
     }
@@ -17,3 +31,22 @@ export default function ImageCard({ onClick, image: { urls: { small }, alt_descr
         </>        
 )
 }
+
+export default ImageCard;
+
+
+// export default function ImageCard: React.FC<ImageCardProps>({ onClick, image: { urls: { small }, alt_description, user: {name} } }) {
+//     const handleClick = () => {
+//         onClick && onClick();
+//     }
+//     return (
+//         <>
+//             <img src={small} alt={alt_description} onClick={handleClick} className={css.img} />
+//             <div className={css.cont}>
+//             <p className={css.text}>  <GoCheck /> Username: {name}</p>
+//             <p className={css.text}> <MdOutlineDescription /> Desription: {alt_description}</p>   
+//             </div>
+            
+//         </>        
+// )
+// }
