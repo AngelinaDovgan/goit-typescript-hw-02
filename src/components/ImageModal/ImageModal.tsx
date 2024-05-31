e4a23d1e1a93beaa1ea08f1eabfa34d02caf727a
Angelina <midnightshout03@gmail.com>
2024-05-31 15:08:53 +0300

    modal

--- src/components/ImageModal/ImageModal.tsx
+++ src/components/ImageModal/ImageModal.tsx
@@ -2,7 +2,20 @@ import ReactModal from "react-modal";
 import css from './ImageModal.module.css'
 ReactModal.setAppElement('#root');
 
-export default function ImageModal({isOpen, onRequestClose, image: {urls: {regular}, alt_description}}) {
+ReactModal.setAppElement('#root');
+
+interface ImageModalProps {
+    isOpen: boolean;
+    onRequestClose: () => void;
+    image: {
+        urls: {
+            regular: string;
+        };
+        alt_description: string;
+    }
+}
+
+const ImageModal: React.FC<ImageModalProps> = ({isOpen, onRequestClose, image: {urls: {regular}, alt_description}}) => {
     return (
         <ReactModal
             isOpen={isOpen}
@@ -18,3 +31,27 @@ export default function ImageModal({isOpen, onRequestClose, image: {urls: {regul
             </ReactModal>
     )
 }
+
+export default ImageModal;
+
+
+
+
+
+
+// export default function ImageModal({isOpen, onRequestClose, image: {urls: {regular}, alt_description}}) {
+//     return (
+//         <ReactModal
+//             isOpen={isOpen}
+//             onRequestClose={onRequestClose}
+//             contentLabel='Modal Window'
+//             shouldCloseOnEsc={true}
+//             shouldCloseOnOverlayClick={true}
+//             className={css.content}
+//             imgClassName={css.img}
+//             overlayClassName={css.overlay}
+//         >
+//             <img src={regular} alt={alt_description} />
+//             </ReactModal>
+//     )
+// }
