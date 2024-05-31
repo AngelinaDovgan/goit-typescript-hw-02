a7bf6ca6589e961573c8df430c19a9788c79df7c
Angelina <midnightshout03@gmail.com>
2024-05-31 15:47:13 +0300

    Update LoadMoreBtn.tsx

--- src/components/LoadMoreBtn/LoadMoreBtn.tsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -1,7 +1,22 @@
 import css from './LoadMoreBtn.module.css'
 
-export default function LoadMoreBtn({ onClick }) {
+interface LoadMoreBtnProps {
+    onClick: () => void;
+}
+
+const  LoadMoreBtn: React.FC<LoadMoreBtnProps> = ({ onClick }) => {
     return (
     <button onClick={onClick} type="button" className={css.btn}>Load more images</button>
     )
 }
+export default LoadMoreBtn;
+
+
+
+
+
+// export default function LoadMoreBtn({ onClick }) {
+//     return (
+//     <button onClick={onClick} type="button" className={css.btn}>Load more images</button>
+//     )
+// }
