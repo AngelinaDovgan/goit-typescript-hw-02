3b2d1710dc3b9deb63824c9810b162d5e7c2ec5e
Angelina <midnightshout03@gmail.com>
2024-05-31 14:50:38 +0300

    Update ImageGallery.tsx

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -1,20 +1,50 @@
 import ImageCard from "../ImageCard/ImageCard"
 import { nanoid } from "nanoid"
 import css from './ImageGallery.module.css'
+import { Image as GalleryImg} from "../App/types"
 
-export default function ImageGallery({ images, openModal }) {
-    const handleImgClick = (image) => {
+interface ImageGalleryProps {
+    images: GalleryImg[];
+    openModal: (image: GalleryImg) => void;
+
+}
+
+const ImageGallery: React.FC<ImageGalleryProps> = ({ images, openModal }) => {
+    const handleImgClick = (image: GalleryImg) => {
         openModal(image);
     }
     return (
         <ul className={css.gallery}>
             {images.map((image) => (
                 <li key={nanoid()} onClick={() => handleImgClick(image)} className={css.imgitem}>
-                    <ImageCard image={image} />
+                    <ImageCard image={image} onClick={() => handleImgClick(image)} />
 </li>
             
             ))}
 </ul>
     )
 };
+
+export default ImageGallery;
+
+
+
+
+
+
+// export default function ImageGallery({ images, openModal }) {
+//     const handleImgClick = (image) => {
+//         openModal(image);
+//     }
+//     return (
+//         <ul className={css.gallery}>
+//             {images.map((image) => (
+//                 <li key={nanoid()} onClick={() => handleImgClick(image)} className={css.imgitem}>
+//                     <ImageCard image={image} />
+// </li>
+            
+//             ))}
+// </ul>
+//     )
+// };
    
